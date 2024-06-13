ab1ac40602fbc2fcad84bbf56b2cab917a9deec8
name <email@email>
2024-06-13 21:10:08 +0300

    FriendsPage-add

--- src/pages/FriendsPage.jsx
+++ src/pages/FriendsPage.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import "../styles/FriendsPage.css";
 
 function FriendsPage() {
   return (
-    <div>
-      <h2>Друзья</h2>
-      <p>Тут что-то будет...</p>
+    <div className="merch-container">
+      <div className="merch-wrapper">
+        <div>
+          <h1>Пригласите друзей</h1>
+        </div>
+        <div>
+          <h2>Вы и ваш друг получите бонусы</h2>
+        </div>
+      </div>
     </div>
   );
 }
